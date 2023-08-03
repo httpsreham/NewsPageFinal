365a15a88b6012fa0fec5702093d0c6fc1b16725
Reham <rehameihab@gmail.com>
2023-08-03 15:09:12 +0100

    Initial Commit

---
+++ NewsPageFinal/NewsPageFinalApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsPageFinalApp.swift
+//  NewsPageFinal
+//
+//  Created by scholar on 03/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct NewsPageFinalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
